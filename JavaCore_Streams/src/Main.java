b127664fd03122a6e6baf4331a825e4c0cc2f434
semyonsavenkov <semyonsavenkov@gmail.com>
2022-11-27 01:10:10 +0600

    class Main fixed class StreamMain changed

--- JavaCore_Streams/src/Main.java
+++ JavaCore_Streams/src/Main.java
@@ -1,26 +1,20 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> intList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
-//        Iterator<Integer> iter = intList.iterator();
-//        while (iter.hasNext()) {
-//            Integer myInt = iter.next();
-//            if (myInt < 0) {
-//                iter.remove();
-//            }
-//        }
-        for (int i = 0; i < intList.size(); i++) {
+        List<Integer> intArrayList = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        LinkedList<Integer> intList = new LinkedList<Integer>();
+        intList.addAll(intArrayList);
+
+        for (int i = intList.size()-1; i >= 0; i--) {
             Integer myInt = intList.get(i);
             if (myInt < 0) {
-//                intList.set(i, 100);
-                intList.remove(i);
+                intList.remove(myInt);
             }
-
+            else if (myInt % 2 != 0) {
+                intList.remove(myInt);
+            }
         }
         System.out.println(intList.toString());
     }
